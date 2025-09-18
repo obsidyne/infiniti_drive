424914db2f6fcf7d3a48ca7d87370108531f4d2a
navadeep satheesh <navadeepsatheesh@gmail.com>
2025-09-18 23:17:58 +0530

    f

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,9 +30,9 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center space-x-2 group">
-            <div className="p-2 bg-gradient-to-r from-green-500 to-blue-500 rounded-lg group-hover:scale-110 transition-transform duration-300">
+            {/* <div className="p-2 bg-gradient-to-r from-green-500 to-blue-500 rounded-lg group-hover:scale-110 transition-transform duration-300">
               <Bike className="h-6 w-6 text-white" />
-            </div>
+            </div> */}
             <span className="text-xl font-bold bg-gradient-to-r from-green-400 to-blue-400 bg-clip-text text-transparent">
               Infiniti Drive
             </span>
